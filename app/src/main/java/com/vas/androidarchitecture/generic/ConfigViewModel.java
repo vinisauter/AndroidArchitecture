29a6bc99598acb7cd4cce777adaa8a97495adba1
vinicius <vsauter@brq.com>
2018-10-31 14:40:19 -0300

    ViewModel Constructor treatment

--- app/src/main/java/com/vas/androidarchitecture/generic/ConfigViewModel.java
+++ app/src/main/java/com/vas/androidarchitecture/generic/ConfigViewModel.java
@@ -1,23 +1,29 @@
 package com.vas.androidarchitecture.generic;
 
+import android.app.Application;
+
 import com.vas.architectureandroidannotations.ViewModelARC;
 import com.vas.architectureandroidannotations.api.Callback;
 import com.vas.architectureandroidannotations.api.TaskStatus;
 import com.vas.architectureandroidannotations.viewmodel.Repository;
 
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.ViewModel;
 
 /**
  * Created by Vinicius Sauter liveData 09/10/2018.
  * .
  */
 @ViewModelARC
-public class ConfigViewModel extends ViewModel {
+public class ConfigViewModel extends AndroidViewModel {
 
     @Repository
     ConfigRepositoryARC repository;
     public final MutableLiveData<TaskStatus> statusTask = new MutableLiveData<>();
+
+    public ConfigViewModel(Application application) {
+        super(application);
+    }
 
     public void serialAsync() {
         repository.serialAsync(new Callback<Void>() {
--- architecture-processor/src/main/java/com/vas/architecture_processor/operations/ArcViewModelGenerator.java
+++ architecture-processor/src/main/java/com/vas/architecture_processor/operations/ArcViewModelGenerator.java
@@ -5,6 +5,7 @@ import com.squareup.javapoet.ClassName;
 import com.squareup.javapoet.FieldSpec;
 import com.squareup.javapoet.JavaFile;
 import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.ParameterSpec;
 import com.squareup.javapoet.TypeName;
 import com.squareup.javapoet.TypeSpec;
 import com.vas.architecture_processor.Utils;
@@ -16,20 +17,22 @@ import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Set;
 
 import javax.annotation.processing.Filer;
 import javax.annotation.processing.Messager;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.ElementKind;
+import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.Modifier;
+import javax.lang.model.element.VariableElement;
 import javax.lang.model.type.TypeMirror;
 import javax.tools.Diagnostic;
 
 import static javax.lang.model.element.Modifier.FINAL;
 import static javax.lang.model.element.Modifier.PRIVATE;
 import static javax.lang.model.element.Modifier.PUBLIC;
-import static javax.lang.model.element.Modifier.STATIC;
 
 /**
  * Created by user liveData 04/10/2018.
@@ -51,8 +54,8 @@ public class ArcViewModelGenerator {
         TypeSpec.Builder navigatorClass = TypeSpec.classBuilder(className)
                 .addModifiers(PUBLIC)
                 .superclass(TypeName.get(type));
-        MethodSpec.Builder constructor = MethodSpec.constructorBuilder()
-                .addModifiers(PUBLIC);
+        MethodSpec.Builder init = MethodSpec.methodBuilder("_init")
+                .addModifiers(PRIVATE);
 
         for (Element elementEnclosed : elementBase.getEnclosedElements()) {
             ElementKind fieldKind = elementEnclosed.getKind();
@@ -92,46 +95,78 @@ public class ArcViewModelGenerator {
                             messager.printMessage(Diagnostic.Kind.ERROR, MessageFormat.format("Element {0}.{1} may not be private.", elementEnclosed.getSimpleName(), elementEnclosed.getEnclosingElement().getSimpleName()));
                         }
 
-                        constructor.addStatement("super.$N = $T.getInstance()", elementEnclosed.getSimpleName(), elementEnclosed.asType());
-
+                        init.addStatement("super.$N = $T.getInstance()", elementEnclosed.getSimpleName(), elementEnclosed.asType());
                     }
                 }
             } else if (elementEnclosed.getKind() == ElementKind.METHOD) {
                 ArcValidators.validateMethod(elementEnclosed);
+            } else if (elementEnclosed.getKind() == ElementKind.CONSTRUCTOR) {
+                if (elementEnclosed.getModifiers().contains(PUBLIC)) {
+                    ExecutableElement executableElement = (ExecutableElement) elementEnclosed;
+                    MethodSpec.Builder constructor = MethodSpec.constructorBuilder()
+                            .addModifiers(PUBLIC);
+                    List<? extends VariableElement> parameterElements = executableElement.getParameters();
+                    ArrayList<ParameterSpec> parameterSpecs = new ArrayList<>();
+                    ArrayList<String> parameterNames = new ArrayList<>();
+                    StringBuilder ps = new StringBuilder();
+                    StringBuilder parametersString = new StringBuilder();
+                    if (parameterElements.size() > 0) {
+                        VariableElement firstParameter = parameterElements.get(0);
+                        String fpName = firstParameter.getSimpleName().toString();
+                        parameterNames.add(fpName);
+                        TypeMirror fpTypeName = firstParameter.asType();
+                        parameterSpecs.add(ParameterSpec.builder(TypeName.get(fpTypeName), fpName).build());
+                        parametersString.append("(").append(fpTypeName).append(") objects[").append(0).append("]");
+                        ps.append("$N");
+                        for (int i = 1; i < parameterElements.size(); i++) {
+                            VariableElement variableElement = parameterElements.get(i);
+                            String pName = variableElement.getSimpleName().toString();
+                            parameterNames.add(fpName);
+                            TypeMirror pTypeName = variableElement.asType();
+                            parameterSpecs.add(ParameterSpec.builder(TypeName.get(pTypeName), pName).build());
+                            parametersString.append(", (").append(pTypeName).append(") objects[").append(i).append("]");
+                            ps.append(", $N");
+                        }
+                    }
+                    constructor.addParameters(parameterSpecs);
+                    constructor.addStatement("super(" + ps + ")", parameterNames.toArray());
+                    constructor.addStatement("_init()");
+                    navigatorClass.addMethod(constructor.build());
+                }
             }
         }
-        navigatorClass.addMethod(constructor.build());
+        navigatorClass.addMethod(init.build());
 
-        navigatorClass.addMethod(MethodSpec.methodBuilder("createInstance")
-                .addModifiers(PUBLIC, STATIC)
-                .addParameter(ClassName.get("androidx.fragment.app", "Fragment"), "fragment")
-                .addStatement("return $T.of(fragment).get($T.class)",
-                        ClassName.get("androidx.lifecycle", "ViewModelProviders"), className)
-                .returns(className)
-                .build());
-        navigatorClass.addMethod(MethodSpec.methodBuilder("createInstance")
-                .addModifiers(PUBLIC, STATIC)
-                .addParameter(ClassName.get("androidx.fragment.app", "FragmentActivity"), "activity")
-                .addStatement("return $T.of(activity).get($T.class)",
-                        ClassName.get("androidx.lifecycle", "ViewModelProviders"), className)
-                .returns(className)
-                .build());
-        navigatorClass.addMethod(MethodSpec.methodBuilder("createInstance")
-                .addModifiers(PUBLIC, STATIC)
-                .addParameter(ClassName.get("androidx.fragment.app", "Fragment"), "fragment")
-                .addParameter(ClassName.get("androidx.lifecycle", "ViewModelProvider", "Factory"), "factory")
-                .addStatement("return $T.of(fragment, factory).get($T.class)",
-                        ClassName.get("androidx.lifecycle", "ViewModelProviders"), className)
-                .returns(className)
-                .build());
-        navigatorClass.addMethod(MethodSpec.methodBuilder("createInstance")
-                .addModifiers(PUBLIC, STATIC)
-                .addParameter(ClassName.get("androidx.fragment.app", "FragmentActivity"), "activity")
-                .addParameter(ClassName.get("androidx.lifecycle", "ViewModelProvider", "Factory"), "factory")
-                .addStatement("return $T.of(activity, factory).get($T.class)",
-                        ClassName.get("androidx.lifecycle", "ViewModelProviders"), className)
-                .returns(className)
-                .build());
+//        navigatorClass.addMethod(MethodSpec.methodBuilder("createInstance")
+//                .addModifiers(PUBLIC, STATIC)
+//                .addParameter(ClassName.get("androidx.fragment.app", "Fragment"), "fragment")
+//                .addStatement("return $T.of(fragment).get($T.class)",
+//                        ClassName.get("androidx.lifecycle", "ViewModelProviders"), className)
+//                .returns(className)
+//                .build());
+//        navigatorClass.addMethod(MethodSpec.methodBuilder("createInstance")
+//                .addModifiers(PUBLIC, STATIC)
+//                .addParameter(ClassName.get("androidx.fragment.app", "FragmentActivity"), "activity")
+//                .addStatement("return $T.of(activity).get($T.class)",
+//                        ClassName.get("androidx.lifecycle", "ViewModelProviders"), className)
+//                .returns(className)
+//                .build());
+//        navigatorClass.addMethod(MethodSpec.methodBuilder("createInstance")
+//                .addModifiers(PUBLIC, STATIC)
+//                .addParameter(ClassName.get("androidx.fragment.app", "Fragment"), "fragment")
+//                .addParameter(ClassName.get("androidx.lifecycle", "ViewModelProvider", "Factory"), "factory")
+//                .addStatement("return $T.of(fragment, factory).get($T.class)",
+//                        ClassName.get("androidx.lifecycle", "ViewModelProviders"), className)
+//                .returns(className)
+//                .build());
+//        navigatorClass.addMethod(MethodSpec.methodBuilder("createInstance")
+//                .addModifiers(PUBLIC, STATIC)
+//                .addParameter(ClassName.get("androidx.fragment.app", "FragmentActivity"), "activity")
+//                .addParameter(ClassName.get("androidx.lifecycle", "ViewModelProvider", "Factory"), "factory")
+//                .addStatement("return $T.of(activity, factory).get($T.class)",
+//                        ClassName.get("androidx.lifecycle", "ViewModelProviders"), className)
+//                .returns(className)
+//                .build());
         // 3- Write generated class to a file
         JavaFile.builder(pack, navigatorClass.build()).build().writeTo(filer);
         return className;
